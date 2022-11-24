d452756bb6c7826146750123db8671894638c0bb
jpromanonet <programming@jpromano.net>
2022-11-24 16:06:33 -0300

    Initial commit

---
+++ addnumber.py
@@ -0,0 +1,7 @@
+firstString = "1111 2222"
+secondString = "2222 1111"
+
+# Let's convert the variables from Strings to Integers
+
+finalResult = int(firstString) + int(secondString)
+print(finalResult)
