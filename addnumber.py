f08326e66cd924715f238bb5c9de8cf6f4c4872b
jpromanonet <programming@jpromano.net>
2022-11-25 16:17:43 -0300

    splitting lists

--- addnumber.py
+++ addnumber.py
@@ -1,7 +1,15 @@
-firstString = "1111"
-secondString = "2222"
+firstString = "1111 5555"
+secondString = "2222 4444"
+
+# Let's create a list from the strings
+def ConvertStrings(firstString, secondString):
+    firstList = list(firstString.split(" "))
+    secondList = list(secondString.split(" "))
+    print(firstList[0])
+    print(secondList[1])
+
+ConvertStrings(firstString, secondString)
 
 # Let's convert the variables from Strings to Integers
-
-finalResult = int(firstString) + int(secondString)
-print(finalResult)
+firstString.split()
+secondString.split()
