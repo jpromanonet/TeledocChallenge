ee81bc9edbcf7ec3a977276ecb44612e604d156b
jpromanonet <programming@jpromano.net>
2022-11-25 17:10:44 -0300

    splitting lists

--- addnumber.py
+++ addnumber.py
@@ -1,18 +1,29 @@
+# Take Home Exercise:
+
+
 # Let's create a list from the strings
 def addNumbers(firstString, secondString):
+    # Let's convert the string parameters into list so every number separated by spaces becomes an index
     firstList = list(firstString.split(" "))
     secondList = list(secondString.split(" "))
+    # Now lest measure the first list to use the index as reference with the other list index when adding numbers later
     length = len(firstList)
+    # Here we iterate over the two lists
     for i in range(length):
         lists = [firstList, secondList]
+        # Converting the list into string to find if there is any negative sign and raise an error if so
+        if str(lists).find("-") != -1:
+            raise ValueError("You're using a negative number and it's not allowed")
         it = iter(lists)
         the_len = len(next(it))
+        # Checking the length of both list by using the iter build-in function
         if not all(len(l) == the_len for l in it):
-            raise ValueError('not all lists have the same length!')
+            raise ValueError('The list are not the same length')
+        # If everything is fine (not negative numbers neither difference in lists lengths) we add the strings
         else:
             print(float(firstList[i])+float(secondList[i]))
 
-
-addNumbers("1234567.8901 2.345", "12.34 2345678901.2")
+# Testing
+addNumbers("1234567.8901 2.345", "12.34 2345678901.2 2")
 print(" ")
 addNumbers("123 456 789", "11 22 33")
